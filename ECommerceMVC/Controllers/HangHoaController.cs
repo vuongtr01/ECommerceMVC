3c304eb645b4df6eebbc8285863ee370c0dc7c50
Luc Vuong <72903985+vuongtr01@users.noreply.github.com>
2024-09-18 17:16:44 -0500

    add search feature

--- ECommerceMVC/Controllers/HangHoaController.cs
+++ ECommerceMVC/Controllers/HangHoaController.cs
@@ -37,6 +37,25 @@ namespace ECommerceMVC.Controllers
             });
             return View(result);
         }
+
+        public IActionResult Search(string? query)
+        {
+            var hangHoas = db.HangHoas.AsQueryable();
+            if (query != null)
+            {
+                hangHoas = hangHoas.Where(p => p.TenHh.Contains(query));
+            }
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHH = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
+        }
     }
 }
 
